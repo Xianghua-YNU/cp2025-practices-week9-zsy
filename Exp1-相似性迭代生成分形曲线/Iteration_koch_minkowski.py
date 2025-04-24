4a81c3a9f90039a079b142192d1ce334f28d1052
zhusiyuv <1477117691@qq.com>
2025-04-24 13:25:24 +0800

    Update Iteration_koch_minkowski.py

--- Exp1-相似性迭代生成分形曲线/Iteration_koch_minkowski.py
+++ Exp1-相似性迭代生成分形曲线/Iteration_koch_minkowski.py
@@ -13,7 +13,21 @@ def koch_generator(u, level):
         numpy.ndarray: 生成的所有点（复数数组）
     """
     # TODO: 实现科赫曲线生成算法
-    pass
+     if level == 0:
+        return u
+    else:
+        a = u[0]
+        b = u[1]
+        c = a + (b - a) / 3
+        d = c + (b - a) * np.exp(1j * np.pi / 3) / 3
+        e = a + 2 * (b - a) / 3
+        new_points = np.array([a, c, d, e, b])
+        return np.concatenate([
+            koch_generator(new_points[:2], level-1),
+            koch_generator(new_points[1:3], level-1),
+            koch_generator(new_points[2:4], level-1),
+            koch_generator(new_points[3:], level-1)
+        ])
 
 def minkowski_generator(u, level):
     """
@@ -27,7 +41,40 @@ def minkowski_generator(u, level):
         numpy.ndarray: 生成的所有点（复数数组）
     """
     # TODO: 实现闵可夫斯基香肠曲线生成算法
-    pass
+    if level == 0:
+        return u
+    else:
+        a = u[0]
+        b = u[1]
+        length = np.abs(b - a)
+        angle = np.angle(b - a)
+        step = length / 8
+        new_points = np.array([
+            a,
+            a + step * np.exp(1j * angle),
+            a + 2 * step * np.exp(1j * angle),
+            a + 3 * step * np.exp(1j * angle),
+            a + 4 * step * np.exp(1j * angle),
+            a + 5 * step * np.exp(1j * angle),
+            a + 6 * step * np.exp(1j * angle),
+            a + 7 * step * np.exp(1j * angle),
+            a + 8 * step * np.exp(1j * angle),
+            b
+        ])
+        segments = [
+            new_points[:2],
+            new_points[1:3],
+            new_points[2:4],
+            new_points[3:5],
+            new_points[4:6],
+            new_points[5:7],
+            new_points[6:8],
+            new_points[7:9]
+        ]
+        result = []
+        for seg in segments:
+            result.append(minkowski_generator(seg, level-1))
+        return np.concatenate(result)
 
 if __name__ == "__main__":
     # 初始线段
@@ -45,6 +92,7 @@ if __name__ == "__main__":
         axs[i//2, i%2].axis('equal')
         axs[i//2, i%2].axis('off')
     plt.tight_layout()
+    plt.savefig('koch_curve.png')
     plt.show()
 
     # 绘制不同层级的闵可夫斯基香肠曲线
@@ -59,4 +107,5 @@ if __name__ == "__main__":
         axs[i//2, i%2].axis('equal')
         axs[i//2, i%2].axis('off')
     plt.tight_layout()
+    plt.savefig('minkowski_sausage.png')
     plt.show()
